2a0801428a6ea68fe9952f7e9e810a8ee61b21af
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2023-01-19 22:55:13 -0300

    feat(mobile): finalizar aplicativo

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView } from "react-native"
-
+import { useNavigation } from "@react-navigation/native"
 import { generateRangeBetweenDates } from "../utils/generate-range-between-dates"
 
 import { Header } from "../components/Header"
@@ -11,6 +11,8 @@ const minimiumSummaryDateSize = 18 * 5
 const amountOfDaysToFill = minimiumSummaryDateSize - datesFromYearStart.length
 
 export function Home() {
+  const { navigate } = useNavigation()
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -29,13 +31,14 @@ export function Home() {
 
       <ScrollView
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingBottom: 100,
-        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
